d42ca58e2e12d1bf9e5af5d26ed5e2f21ec34845
Alyssa Khoury <alykhou@bgsu.edu>
2024-03-18 18:13:02 -0400

    Some changes to be continued on GameScreen.kt.

--- app/src/main/java/com/example/cs3180_sp2024_g04/GameScreen.kt
+++ app/src/main/java/com/example/cs3180_sp2024_g04/GameScreen.kt
@@ -1,66 +1,76 @@
 package com.example.cs3180_sp2024_g04
 
+import android.widget.Button
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 
 @Composable
 fun flashCard(modifier: Modifier = Modifier, operator: Boolean, operand1:Int, operand2:Int){
-    Column (modifier = Modifier,
-        horizontalAlignment = Alignment.End){
-        Row(
-            modifier = Modifier,
-        )
-        {
-            Text(modifier = Modifier,
-                text = operand1.toString()
-            )
-        }
-        Row {
 
-            Row() {
-                if (operator) {
-                    Text(text = "+")
-                }
-                else {
-                    Text(text = "-")
-                }
-
+        Column(modifier = Modifier
+            .fillMaxWidth()
+            .padding(50.dp),
+            horizontalAlignment = Alignment.End) {
+            Row(modifier = Modifier,) {
+                Text(modifier = Modifier,text = operand1.toString(), fontSize = 50.sp
+                    )
             }
-            Spacer(Modifier.width(7.dp))
-
-            Row() {
-                Text(text = operand2.toString())
-
+            Row {
+                Row() {
+                    if (operator) {
+                        Text(text = "+", fontSize = 50.sp)
+                    } else {
+                        Text(text = "-", fontSize = 50.sp)
+                    }
+                }
+                Spacer(Modifier.width(20.dp))
+                Row() {
+                    Text(text = operand2.toString(), fontSize = 50.sp)
+                }
             }
         }
-
-
     }
 
-}
 
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, device = "id:Nexus One", showSystemUi = true)
 @Composable
 fun gamePreview(){
-    flashCard(operator = true, operand1 = 60, operand2 = 10)
+    ShowMathProblem()
 }
 
 @Composable
 fun ShowMathProblem (AddOrSub: Boolean = true, MaxValue: Int = 10) {
     var Op1 = getRandomNumber(MaxValue)
     var Op2 = getRandomNumber(MaxValue)
-    flashCard(Modifier, AddOrSub, Op1, Op2)
+
+
+    Column(modifier = Modifier, horizontalAlignment = Alignment.CenterHorizontally ) {
+        flashCard(Modifier, AddOrSub, Op1, Op2)
+
+        Button(onClick = { /*TODO*/ }) {
+
+
+        }
+    }
 }
 
 
@@ -70,3 +80,4 @@ fun getRandomNumber(MaxValue: Int = 10): Int {
     var anInt = (randomValues).toInt()
     return anInt
 }
+
